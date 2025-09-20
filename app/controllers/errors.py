034da423e0e3c4806a4f2dd42fc6d6f4352460bc
asdfmake <makevic2003@gmail.com>
2025-09-20 23:51:49 +0200

    Feat: check if login forms have all needed fields and if request body is valid

--- app/controllers/errors.py
+++ app/controllers/errors.py
@@ -2,3 +2,6 @@ from flask import jsonify
 
 def missing_params_error(param: str):
     return jsonify({"missing param": param}), 400
+
+def missing_field_error(field: str):
+    return jsonify({"missing field": field}), 400
